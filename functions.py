fabc7c9af4822802286e94f6206297dd30cba869
KPG <hybridremix@yahoo.com>
2017-12-19 09:45:22 -0500

    Updates from M4 "Adding Students to Our App"

--- functions.py
+++ functions.py
@@ -3,7 +3,7 @@ students = []
 def get_students_titlecase():
     students_titlecase = []
     for student in students:
-        students_titlecase = student.title()
+        students_titlecase = student["name"].title()
     return students_titlecase
 
 def print_students_titlecase():
@@ -12,14 +12,12 @@ def print_students_titlecase():
 
 def add_student(name, student_id=332):
     student = {"name": name, "student_id": student_id}
-    students.append(name)
-
-def var_args(name, **kwargs):
-    print(name)
-    print(kwargs["description"], kwargs["feedback"])
+    students.append(student)
 
 student_list = get_students_titlecase()
 
-add_student(name="Mark", student_id=15)
+student_name = input("Enter student name: ")
+student_id = input("Enter student ID: ")
 
-var_args("Mark", description="Loves Python", feedback=None, pluralsight_subscriber=True)
+add_student(student_name, student_id)
+print_students_titlecase()
