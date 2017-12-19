0476646d28c3380d5ddd849d87ea1912a2c334f6
KPG <hybridremix@yahoo.com>
2017-12-19 11:44:03 -0500

    Updates from M5 "Constructor and Other Special Methods"

--- classes.py
+++ classes.py
@@ -1,11 +1,13 @@
 students = []
 
 class Student:
-    def add_student(self, name, student_id=332):
+    def __init__(self, name, student_id=332):
         student = {"name": name, "student_id": student_id}
         students.append(student)
 
-student = Student()
-student.add_student("Mark")
+    def __str__(self):
+        return "Student"
 
-print(students)
+mark = Student("Mark")
+
+print(mark)
