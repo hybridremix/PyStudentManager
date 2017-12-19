f2209266977ee2377934062da9db77493c58ba24
KPG <hybridremix@yahoo.com>
2017-12-19 09:59:10 -0500

    Updates from M4 "Opening, Reading, and Writing"

--- functions.py
+++ functions.py
@@ -3,7 +3,7 @@ students = []
 def get_students_titlecase():
     students_titlecase = []
     for student in students:
-        students_titlecase = student["name"].title()
+        students_titlecase.append(student["name"].title())
     return students_titlecase
 
 def print_students_titlecase():
@@ -14,10 +14,28 @@ def add_student(name, student_id=332):
     student = {"name": name, "student_id": student_id}
     students.append(student)
 
-student_list = get_students_titlecase()
+def save_file(student):
+    try:
+        f = open("students.txt", "a")
+        f.write(student + "\n")
+        f.close()
+    except Exception:
+        print("Could not save file")
+
+def read_file():
+    try:
+        f = open("students.txt", "r")
+        for student in f.readlines():
+            add_student(student)
+            f.close()
+    except Exception:
+        print("Could not read file")
+
+read_file()
+print_students_titlecase()
 
 student_name = input("Enter student name: ")
 student_id = input("Enter student ID: ")
 
 add_student(student_name, student_id)
-print_students_titlecase()
+save_file(student_name)
